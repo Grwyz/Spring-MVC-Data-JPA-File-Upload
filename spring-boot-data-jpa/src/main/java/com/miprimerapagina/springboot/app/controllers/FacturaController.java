088f7ee0370a7375b6b955b025586e255a8aca0b
Axel Beny Hernandez Martinez <demonraker@gmail.com>
2023-07-08 13:17:20 -0600

    Implementando método handler guardar en FacturaController (181)

--- spring-boot-data-jpa/src/main/java/com/miprimerapagina/springboot/app/controllers/FacturaController.java
+++ spring-boot-data-jpa/src/main/java/com/miprimerapagina/springboot/app/controllers/FacturaController.java
@@ -3,17 +3,23 @@ package com.miprimerapagina.springboot.app.controllers;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.miprimerapagina.springboot.app.models.entity.Cliente;
 import com.miprimerapagina.springboot.app.models.entity.Factura;
+import com.miprimerapagina.springboot.app.models.entity.ItemFactura;
 import com.miprimerapagina.springboot.app.models.entity.Producto;
 import com.miprimerapagina.springboot.app.models.service.IClienteService;
 
@@ -21,6 +27,8 @@ import com.miprimerapagina.springboot.app.models.service.IClienteService;
 @RequestMapping("/factura")
 @SessionAttributes("factura")
 public class FacturaController {
+	
+	private final Logger log = LoggerFactory.getLogger(getClass());
 	
 	@Autowired
 	private IClienteService clienteService;
@@ -48,6 +56,35 @@ public class FacturaController {
 	@GetMapping(value="/cargar-productos/{term}", produces= {"application/json"})
 	public @ResponseBody List<Producto> cargarProductos(@PathVariable String term){
 		return clienteService.findByName(term);
+	}
+	
+	//Guardar la factura
+	@PostMapping("/form")
+	public String guardar(Factura factura,
+			@RequestParam(name="item_id[]", required = false) Long [] itemId,
+			@RequestParam(name="cantidad[]", required = false) Integer[] cantidad,
+			RedirectAttributes flash,
+			SessionStatus status) {
+		
+		//Iteramos cada linea y las agregamos a la factura
+		for(int i = 0; i < itemId.length; i++) {
+			Producto producto = clienteService.findProductoById(itemId[i]);
+			
+			ItemFactura linea = new ItemFactura();
+			linea.setCantidad(cantidad[i]);
+			linea.setProducto(producto);
+			factura.addItemFactura(linea);
+			
+			log.info("ID: " + itemId[i].toString() + ", Cantidad: " + cantidad[i].toString());
+		}
+		
+		//Guardamos la factura
+		clienteService.saveFactura(factura);
+		status.setComplete();
+		
+		flash.addFlashAttribute("success", "Factura creada con éxito!");
+		
+		return "redirect:/ver/" + factura.getCliente().getId();
 	}
 
 }
