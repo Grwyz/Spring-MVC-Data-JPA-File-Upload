b4d4763ea4cbe6b9a69f42e665d175f0f901cab5
Axel Beny Hernandez Martinez <demonraker@gmail.com>
2023-07-09 22:46:04 -0600

    Añadiendo los links del login y logout en el layout para iniciar y cerrar sesión (206)
    - Modificaciones al controlador "LoginController"
    * Agregada validación para saber si se ha cerrado una sesión antes de llamar a la vista "login" (Se agregó un String de nombre "logout" que recibe un valor a través de la anotación "@RequestParam".
    * Añadidos múltiples comentarios respecto al funcionamiento del método y los diversos bloques de código.
    
    -Modificaciones al documento "layout"
    * Añadido el xmlsn de Spring Security.
    * Añadido botón para iniciar sesión (Sign in) con múltiples validaciones.
    * Añadido elemento "dropdown": Se muestra en lugar del botón "Sign In" cuando ya hay una sesión iniciada; muestra el nombre de usuario y despliega un formulario que contiene el botón "Sign Out".
    * Añadido botón "Sign Out" al dropdown: El evento "onclick" hace uso de código js para enviar el formulario "logoutForm".

--- spring-boot-data-jpa/src/main/java/com/miprimerapagina/springboot/app/controllers/LoginController.java
+++ spring-boot-data-jpa/src/main/java/com/miprimerapagina/springboot/app/controllers/LoginController.java
@@ -11,19 +11,29 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 public class LoginController {
 	
+	//Método para mostrar la vista "login"
 	@GetMapping("/login")
 	public String login(@RequestParam(value="error", required = false) String error,
+			@RequestParam(value="logout", required = false) String logout,
 			Model model, Principal principal, RedirectAttributes flash) {
 		
+		//Validamos si el objeto "principal" no es nulo (Indica que ya hay una sesión iniciada)
 		if(principal != null) {
 			flash.addFlashAttribute("info", "Ya ha iniciado sesión anteriormente");
 			return "redirect:/";
 		}
 		
+		//Validamos si el objeto "error" no es nulo (Indica que hay un error en el usuario o la contraseña)
 		if(error != null) {
 			model.addAttribute("error", "Error: Username o Password incorrecto");
 		}
 		
+		//Validamos si el objeto "logout" no es nulo (Indica que se cerró una sesión antes de traer la vista "/login")
+		if(logout != null) {
+			model.addAttribute("success", "Ha cerrado sesión con éxito!");
+		}
+		
+		//Regresamos la vista "login"
 		return "login";
 	}
 }
