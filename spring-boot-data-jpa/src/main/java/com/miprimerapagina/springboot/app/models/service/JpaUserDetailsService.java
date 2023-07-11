936d8eb98381955d0d7ae90bdb8808083cab398e
Axel Beny Hernandez Martinez <demonraker@gmail.com>
2023-07-11 16:32:30 -0600

    Creando la clase de servicio JpaUserDetailsService (223)

---
+++ spring-boot-data-jpa/src/main/java/com/miprimerapagina/springboot/app/models/service/JpaUserDetailsService.java
@@ -0,0 +1,54 @@
+package com.miprimerapagina.springboot.app.models.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.miprimerapagina.springboot.app.models.dao.IUsuarioDao;
+import com.miprimerapagina.springboot.app.models.entity.Role;
+import com.miprimerapagina.springboot.app.models.entity.Usuario;
+
+@Service("jpaUserDetailsService")
+public class JpaUserDetailsService implements UserDetailsService{
+	
+	@Autowired
+	private IUsuarioDao usuarioDao;
+	
+	protected final Log logger = LogFactory.getLog(JpaUserDetailsService.class);
+	
+	@Override
+	@Transactional(readOnly = true)
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Usuario usuario = usuarioDao.findByUsername(username);
+		
+		if(usuario == null) {
+			logger.error("Error: No existe el usuario '" + username + "'.");
+			throw new UsernameNotFoundException("Username " + username + " no existe en el sistema!");
+		}
+		
+		List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+		
+		for(Role role: usuario.getRoles()) {
+			authorities.add(new SimpleGrantedAuthority(role.getAuthority()));
+		}
+		
+		if(authorities.isEmpty()) {
+			logger.error("Error: usuario '" + username + "' no tiene roles asignados!");
+			throw new UsernameNotFoundException("Error: usuario '" + username + "' no tiene roles asignados!");
+		}
+		
+		return new User(usuario.getUsername(), usuario.getPassword(), usuario.getEnabled(), true, true, true, authorities);
+	}
+
+}
