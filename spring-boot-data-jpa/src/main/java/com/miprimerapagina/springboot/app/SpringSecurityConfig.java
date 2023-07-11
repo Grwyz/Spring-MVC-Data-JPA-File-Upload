766df84962fc216995ff065695cc2b6df573ac42
Axel Beny Hernandez Martinez <demonraker@gmail.com>
2023-07-11 01:17:22 -0600

    Configurando JDBC Authentication (219)
    - Modificaciones a la clase "SpringSecurityConfig"
    * Se ha removido el Bean "userDetailsService".
    * Se realizó la inyección de la dependencia "dataSource".
    * Se modificó el nivel de acceso del Bean "filterChain" (era público por error).
    * Cambios menores en el formato del Bean "filterChain" (específicamente la sección comentada).
    * Creación del Bean "authManager" (autenticación mediante el uso de "jdbcAuthentication").
    
    IMPORTANTE: EN EL BEAN "authManager" SE HIZO USO DE UN MÉTODO DEPRECADO Y MARCADO PARA SU ELIMINACIÓN (el método "and()"), POR LO QUE EL MÉTODO REQUIERE SER ACTUALIZADO A LA BREVEDAD. SE HAN AGREGADO ENLACES DE INTERÉS PARA SOLUCIONAR LA INCIDENCIA.

--- spring-boot-data-jpa/src/main/java/com/miprimerapagina/springboot/app/SpringSecurityConfig.java
+++ spring-boot-data-jpa/src/main/java/com/miprimerapagina/springboot/app/SpringSecurityConfig.java
@@ -1,75 +1,62 @@
 package com.miprimerapagina.springboot.app;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.miprimerapagina.springboot.app.auth.handler.LoginSuccessHandler;
 
-import org.springframework.security.core.userdetails.User;
-
 @EnableMethodSecurity(securedEnabled = true, prePostEnabled = true)
 @Configuration
 public class SpringSecurityConfig {
-	
+
 	@Autowired
 	private LoginSuccessHandler successHandler;
-	
+
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
 
-
-	@Bean
-	public UserDetailsService userDetailsService() throws Exception {
-
-		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-
-		// Creamos dos nuevos usuarios: Uno con el rol "USER" y otro con los roles
-		// "USER" y "ADMIN"
-		manager.createUser(
-				User.withUsername("miguel").password(this.passwordEncoder.encode("12345")).roles("USER").build());
-
-		manager.createUser(
-				User.withUsername("admin").password(this.passwordEncoder.encode("admin")).roles("ADMIN", "USER").build());
-
-		return manager;
-	}
+	@Autowired
+	private DataSource dataSource;
 
 	// Método que configura las reglas de autorización y autenticación
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) {
+	SecurityFilterChain filterChain(HttpSecurity http) {
 		// Todo se envuelve en un bloque "try-catch" en caso de haber algún error
 		// durante la configuración
 		try {
 			http.authorizeHttpRequests((authz) -> authz
 					// Todos pueden acceder a la vista principal
 					.requestMatchers("/", "/css/**", "/js/**", "/images/**", "/listar").permitAll()
-					// Todos aquellos quienes tengan el rol "USER" o superior podrán acceder a estas
-					// vistas
-					/*.requestMatchers("/uploads/**").hasAnyRole("USER")*/
-					/*.requestMatchers("/ver/**").hasRole("USER")*/
-					// Solo aquellos que tengan el rol "ADMIN" podrán acceder a estas vistas
-					/*.requestMatchers("/factura/**").hasRole("ADMIN")*/
-					/*.requestMatchers("/form/**").hasRole("ADMIN")*/
-					/*.requestMatchers("/eliminar/**").hasRole("ADMIN")*/
-					// Cualquier petición deberá ser autenticada
+					/*
+					 * // Todos aquellos quienes tengan el rol "USER" o superior podrán acceder a
+					 * estas // vistas .requestMatchers("/uploads/**").hasAnyRole("USER")
+					 * .requestMatchers("/ver/**").hasRole("USER") // Solo aquellos que tengan el
+					 * rol "ADMIN" podrán acceder a estas vistas
+					 * .requestMatchers("/factura/**").hasRole("ADMIN")
+					 * .requestMatchers("/form/**").hasRole("ADMIN")
+					 * .requestMatchers("/eliminar/**").hasRole("ADMIN") // Cualquier petición
+					 * deberá ser autenticada
+					 */
 					.anyRequest().authenticated())
 					// Cualquier persona puede acceder a la vista para iniciar sesión
 					.formLogin((formLogin) -> formLogin.successHandler(successHandler).loginPage("/login").permitAll())
 					// Cualquier persona que haya iniciado sesión puede acceder a la vista para
 					// cerrar sesión
 					.logout((logout) -> logout.permitAll())
-					
+
 					// Manejo de excepciones de acceso denegado
 					.exceptionHandling((exceptionHandling) -> exceptionHandling
 							.accessDeniedHandler((request, response, accessDeniedException) -> {
-								//Redirigimos a una página de error personalizada
+								// Redirigimos a una página de error personalizada
 								response.sendRedirect("/error_403");
 							}));
 
@@ -85,21 +72,21 @@ public class SpringSecurityConfig {
 		}
 	}
 
-	/*
-	 * @Bean public SecurityFilterChain filterChain(HttpSecurity http) throws
-	 * Exception { http.authorizeHttpRequests((authz) -> { try {
-	 * authz.requestMatchers("/", "/css/**", "/js/**", "/images/**",
-	 * "/listar").permitAll()
-	 * .requestMatchers("/uploads/**").hasAnyRole("USER").requestMatchers("/ver/**")
-	 * .hasRole("USER")
-	 * .requestMatchers("/factura/**").hasRole("ADMIN").requestMatchers("/form/**").
-	 * hasRole("ADMIN")
-	 * .requestMatchers("/eliminar/**").hasRole("ADMIN").anyRequest().authenticated(
-	 * ); } catch (Exception e) { e.printStackTrace(); } });
-	 * 
-	 * return http.build();
-	 * 
-	 * }
-	 */
+	// USO DE UN MÉTODO DEPRECADO - REVISAR Y ACTUALIZAR
+	@Bean
+	AuthenticationManager authManager(HttpSecurity http) throws Exception {
+		return http.getSharedObject(AuthenticationManagerBuilder.class).jdbcAuthentication().dataSource(dataSource)
+				.passwordEncoder(passwordEncoder)
+				.usersByUsernameQuery("select username, password, enabled from users where username=?")
+				.authoritiesByUsernameQuery(
+						"select u.username, a.authority from authorities a inner join users u on (a.user_id=u.id) where u.username=?")
+				.and().build();
+	}
+	
+	/* Enlaces de interés:
+	 * https://es.stackoverflow.com/questions/597594/cuál-es-la-alternativa-a-la-función-and-que-aparece-como-deprecated
+	 * https://docs.spring.io/spring-security/site/docs/current/api/org/springframework/security/config/annotation/SecurityConfigurerAdapter.html#and()
+	 * https://spring.io/blog/2019/11/21/spring-security-lambda-dsl
+	*/
 
 }
