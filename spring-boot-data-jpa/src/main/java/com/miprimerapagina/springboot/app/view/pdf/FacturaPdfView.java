4fa01758947356dbd5b8b349d2739acc50f80799
Axel Beny Hernandez Martinez <demonraker@gmail.com>
2023-07-14 15:25:06 -0600

    Creando la vista PdfView para la factura (239)

---
+++ spring-boot-data-jpa/src/main/java/com/miprimerapagina/springboot/app/view/pdf/FacturaPdfView.java
@@ -0,0 +1,44 @@
+package com.miprimerapagina.springboot.app.view.pdf;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.document.AbstractPdfView;
+
+import com.lowagie.text.Document;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
+import com.miprimerapagina.springboot.app.models.entity.Factura;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component("factura/ver")
+public class FacturaPdfView extends AbstractPdfView{
+
+	@Override
+	protected void buildPdfDocument(Map<String, Object> model, Document document, PdfWriter writer,
+			HttpServletRequest request, HttpServletResponse response) throws Exception {
+			
+		Factura factura = (Factura) model.get("factura");
+		
+		//Creamos la primera tabla que contendrá los datos del cliente
+		PdfPTable tabla = new PdfPTable(1);
+		tabla.addCell("Datos del Cliente");
+		tabla.addCell(factura.getCliente().getNombre() + " " + factura.getCliente().getApellido());
+		tabla.addCell(factura.getCliente().getEmail());
+		
+		//Creamos la segunda tabla que contendrá los datos de la factura
+		PdfPTable tabla2 = new PdfPTable(1);
+		tabla.addCell("Datos de la Factura");
+		tabla.addCell("Folio: " + factura.getId());
+		tabla.addCell("Descripción: " + factura.getDescripcion());
+		tabla.addCell("Fecha: " + factura.getCreateAt());
+		
+		//Agregamos las dos tablas al documento
+		document.add(tabla);
+		document.add(tabla2);
+		
+	}
+
+}
