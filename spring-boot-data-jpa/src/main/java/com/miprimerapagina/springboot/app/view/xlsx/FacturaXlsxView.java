07f38556095e6875ee2f525df3cac745190bdf66
Axel Beny Hernandez Martinez <demonraker@gmail.com>
2023-07-17 13:28:06 -0600

    Escribiendo el código del detalle de la Factura en la vista Excel (248)

--- spring-boot-data-jpa/src/main/java/com/miprimerapagina/springboot/app/view/xlsx/FacturaXlsxView.java
+++ spring-boot-data-jpa/src/main/java/com/miprimerapagina/springboot/app/view/xlsx/FacturaXlsxView.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.document.AbstractXlsxView;
 
 import com.miprimerapagina.springboot.app.models.entity.Factura;
+import com.miprimerapagina.springboot.app.models.entity.ItemFactura;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -44,13 +45,25 @@ public class FacturaXlsxView extends AbstractXlsxView{
 		sheet.getRow(8).createCell(1).setCellValue(factura.getCreateAt());
 		
 		String[] titulos = {"Producto", "Precio", "Cantidad", "Total"};
-		
 		sheet.createRow(10);
 		
 		for(int i = 0; i < titulos.length; i++) {
 			String titulo = titulos[i];
 			sheet.getRow(10).createCell(i).setCellValue(titulo);
 		}
+		
+		int rownum = 11;
+		for(ItemFactura item: factura.getItems()) {
+			Row fila = sheet.createRow(rownum++);
+			fila.createCell(0).setCellValue(item.getProducto().getNombre());
+			fila.createCell(1).setCellValue(item.getProducto().getPrecio());
+			fila.createCell(2).setCellValue(item.getCantidad());
+			fila.createCell(3).setCellValue(item.calcularImporte());
+		}
+		
+		Row filaTotal = sheet.createRow(rownum);
+		filaTotal.createCell(2).setCellValue("Gran Total: ");
+		filaTotal.createCell(3).setCellValue(factura.getTotal());
 		
 		
 		
