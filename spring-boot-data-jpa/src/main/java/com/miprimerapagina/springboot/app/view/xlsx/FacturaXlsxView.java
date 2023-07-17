de99f32e5112ce916bd7474d6239a6cb231ab497
Axel Beny Hernandez Martinez <demonraker@gmail.com>
2023-07-17 13:14:54 -0600

    Implementando la clase de vista FacturaXlsxView para la factura (247)

--- spring-boot-data-jpa/src/main/java/com/miprimerapagina/springboot/app/view/xlsx/FacturaXlsxView.java
+++ spring-boot-data-jpa/src/main/java/com/miprimerapagina/springboot/app/view/xlsx/FacturaXlsxView.java
@@ -1,5 +1,70 @@
 package com.miprimerapagina.springboot.app.view.xlsx;
 
-public class FacturaXlsxView {
+import java.util.Map;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.document.AbstractXlsxView;
+
+import com.miprimerapagina.springboot.app.models.entity.Factura;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component("factura/ver.xlsx")
+public class FacturaXlsxView extends AbstractXlsxView{
+
+	@Override
+	protected void buildExcelDocument(Map<String, Object> model, Workbook workbook, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+			
+		Factura factura = (Factura) model.get("factura");
+		Sheet sheet = workbook.createSheet("Factura Spring");
+		
+		Row row = sheet.createRow(0);
+		Cell cell = row.createCell(0);
+		cell.setCellValue("Datos del cliente");
+		
+		sheet.createRow(1).createCell(0).setCellValue("Nombre: ");
+		sheet.getRow(1).createCell(1).setCellValue(factura.getCliente().getNombre());
+		sheet.createRow(2).createCell(0).setCellValue("Apellido: ");
+		sheet.getRow(2).createCell(1).setCellValue(factura.getCliente().getApellido());
+		sheet.createRow(3).createCell(0).setCellValue("Email: ");
+		sheet.getRow(3).createCell(1).setCellValue(factura.getCliente().getEmail());
+		
+		sheet.createRow(5).createCell(0).setCellValue("Datos de la factura");
+		sheet.createRow(6).createCell(0).setCellValue("Folio: ");
+		sheet.getRow(6).createCell(1).setCellValue(factura.getId());
+		sheet.createRow(7).createCell(0).setCellValue("Descripción: ");
+		sheet.getRow(7).createCell(1).setCellValue(factura.getDescripcion());
+		sheet.createRow(8).createCell(0).setCellValue("Fecha: ");
+		sheet.getRow(8).createCell(1).setCellValue(factura.getCreateAt());
+		
+		String[] titulos = {"Producto", "Precio", "Cantidad", "Total"};
+		
+		sheet.createRow(10);
+		
+		for(int i = 0; i < titulos.length; i++) {
+			String titulo = titulos[i];
+			sheet.getRow(10).createCell(i).setCellValue(titulo);
+		}
+		
+		
+		
+		/*
+		sheet.createRow(10).createCell(0).setCellValue("Producto");
+		sheet.getRow(10).createCell(1).setCellValue("Precio");
+		sheet.getRow(10).createCell(2).setCellValue("Cantidad");
+		sheet.getRow(10).createCell(3).setCellValue("Total");
+		*/
+		
+		
+		
+		
+		
+	}
 
 }
