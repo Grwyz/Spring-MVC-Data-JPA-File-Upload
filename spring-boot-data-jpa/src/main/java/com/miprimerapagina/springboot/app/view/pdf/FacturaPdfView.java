c7aca7030ab4f5a541ec96be67fd737aefd7d264
Axel Beny Hernandez Martinez <demonraker@gmail.com>
2023-07-17 20:01:35 -0600

    Modificaciones a la clase "FacturaPdfView"
    * Mejora del diseño (se agregaron bordes en la parte superior e inferior de la página.
    * Se redujo en gran medida el tamaño de las barras que dividían las secciones de la factura.
    * Se removió el prototipo inicial de vista (método que, en versiones anteriores, se encontraba comentado).

--- spring-boot-data-jpa/src/main/java/com/miprimerapagina/springboot/app/view/pdf/FacturaPdfView.java
+++ spring-boot-data-jpa/src/main/java/com/miprimerapagina/springboot/app/view/pdf/FacturaPdfView.java
@@ -1,34 +1,24 @@
 package com.miprimerapagina.springboot.app.view.pdf;
 
 import java.awt.Color;
-import java.io.IOException;
-import java.util.Locale;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.context.support.MessageSourceAccessor;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.view.document.AbstractPdfView;
 
 import com.lowagie.text.Chunk;
 import com.lowagie.text.Document;
-import com.lowagie.text.DocumentException;
 import com.lowagie.text.Element;
 import com.lowagie.text.Font;
 import com.lowagie.text.FontFactory;
-import com.lowagie.text.Image;
 import com.lowagie.text.PageSize;
 import com.lowagie.text.Paragraph;
 import com.lowagie.text.Phrase;
 import com.lowagie.text.Rectangle;
-import com.lowagie.text.pdf.PdfCell;
+import com.lowagie.text.pdf.PdfContentByte;
 import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
-import com.lowagie.text.pdf.PdfPageEventHelper;
 import com.lowagie.text.pdf.PdfWriter;
 import com.miprimerapagina.springboot.app.models.entity.Factura;
 import com.miprimerapagina.springboot.app.models.entity.ItemFactura;
@@ -38,96 +28,6 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component("factura/ver")
 public class FacturaPdfView extends AbstractPdfView {
-
-	@Autowired
-	private MessageSource messageSource;
-
-	@Autowired
-	private LocaleResolver localeResolver;
-	
-	/*
-	@Override
-	protected void buildPdfDocument(Map<String, Object> model, Document document, PdfWriter writer,
-			HttpServletRequest request, HttpServletResponse response) throws Exception {
-
-		Factura factura = (Factura) model.get("factura");
-
-		Locale locale = localeResolver.resolveLocale(request);
-
-		MessageSourceAccessor mensajes = getMessageSourceAccessor();
-
-		// Creamos la primera tabla que contendrá los datos del cliente PdfPTable
-		PdfPTable tabla = new PdfPTable(1);
-		tabla.setSpacingAfter(20);
-
-		PdfPCell cell = null;
-
-		cell = new PdfPCell(new Phrase(messageSource.getMessage("text.factura.ver.datos.cliente", null, locale)));
-		cell.setBackgroundColor(new Color(156, 137, 184));
-		cell.setPadding(8f);
-		tabla.addCell(cell);
-
-		tabla.addCell(factura.getCliente().getNombre() + " " + factura.getCliente().getApellido());
-		tabla.addCell(factura.getCliente().getEmail());
-
-		// Creamos la segunda tabla que contendrá los datos de la factura PdfPTable
-		PdfPTable tabla2 = new PdfPTable(1);
-		tabla2.setSpacingAfter(20);
-
-		cell = new PdfPCell(new Phrase(messageSource.getMessage("text.factura.ver.datos.factura", null, locale)));
-		cell.setBackgroundColor(new Color(156, 137, 184));
-		cell.setPadding(8f);
-
-		tabla2.addCell(cell);
-		tabla2.addCell(mensajes.getMessage("text.cliente.factura.folio") + ": " + factura.getId());
-		tabla2.addCell(mensajes.getMessage("text.cliente.factura.descripcion") + ": " + factura.getDescripcion());
-		tabla2.addCell(mensajes.getMessage("text.cliente.factura.fecha") + ": " + factura.getCreateAt());
-
-		// Agregamos las dos tablas al documento document.add(tabla);
-		document.add(tabla);
-		document.add(tabla2);
-
-		PdfPTable tabla3 = new PdfPTable(4);
-		tabla3.setWidths(new float[] { 2.5f, 1, 1, 1 });
-
-		String[] titulos = { mensajes.getMessage("text.factura.form.item.nombre"),
-				mensajes.getMessage("text.factura.form.item.precio"),
-				mensajes.getMessage("text.factura.form.item.cantidad"),
-				mensajes.getMessage("text.factura.form.item.total") };
-
-		for (String titulo : titulos) {
-			cell = new PdfPCell(new Phrase(titulo));
-			cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-			cell.setBackgroundColor(new Color(156, 137, 184));
-			tabla3.addCell(cell);
-		}
-
-		for (ItemFactura item : factura.getItems()) {
-			tabla3.addCell(item.getProducto().getNombre());
-
-			PdfPCell precioCell = new PdfPCell(new Phrase(item.getProducto().getPrecio().toString()));
-			precioCell.setHorizontalAlignment(Element.ALIGN_CENTER);
-			tabla3.addCell(precioCell);
-
-			PdfPCell cantidadCell = new PdfPCell(new Phrase(item.getCantidad().toString()));
-			cantidadCell.setHorizontalAlignment(Element.ALIGN_CENTER);
-			tabla3.addCell(cantidadCell);
-
-			PdfPCell importeCell = new PdfPCell(new Phrase(item.calcularImporte().toString()));
-			importeCell.setHorizontalAlignment(Element.ALIGN_CENTER);
-			tabla3.addCell(importeCell);
-		}
-
-		cell = new PdfPCell(new Phrase(mensajes.getMessage("text.factura.form.total") + ": "));
-		cell.setColspan(3);
-		cell.setHorizontalAlignment(PdfCell.ALIGN_RIGHT);
-		tabla3.addCell(cell);
-		tabla3.addCell(factura.getTotal().toString());
-
-		document.add(tabla3);
-
-	}
-	*/
 
 	@Override
 	protected void buildPdfDocument(Map<String, Object> model, Document document, PdfWriter writer,
@@ -139,18 +39,32 @@ public class FacturaPdfView extends AbstractPdfView {
 		
 		MessageSourceAccessor mensajes = getMessageSourceAccessor();
 
+		/*
 		// Configuración del documento PDF
 		document.setPageSize(PageSize.A4);
 		document.setMargins(40, 40, 40, 40);
 		document.addTitle("Factura");
+		*/
 		
-	    // Agregar barra
-	 	PdfPTable barra = new PdfPTable(1);
-	 	barra.setWidthPercentage(200);
-	 	barra.getDefaultCell().setBackgroundColor(new Color(156, 137, 184));
-	 	barra.getDefaultCell().setFixedHeight(10);
-	 	barra.addCell("");
-	 	document.add(barra);
+		float marginHeight = 40;
+		// Se obtiene el tamaño de la página
+        float pageWidth = document.getPageSize().getWidth();
+        float pageHeight = document.getPageSize().getHeight();
+        
+        // Se obtiene un objeto para dibujar en el documento
+        PdfContentByte contentByte = writer.getDirectContent();
+        // Se establece el color con el que se rellenarán los rectángulos de los bordes superior e inferior
+        contentByte.setColorFill(new Color(156, 137, 184));
+        // Rectángulo borde superior
+        contentByte.rectangle(0, pageHeight - marginHeight, pageWidth, marginHeight);
+        // Rectángulo borde inferior
+        contentByte.rectangle(0, 0, pageWidth, marginHeight);
+        // Se rellenan los rectángulos
+        contentByte.fill();
+        
+        // Establecer una fuente de color negro
+        Font font = new Font();
+        font.setColor(Color.BLACK);
 		
 		// Agregar el título "Factura"
 		Paragraph tituloFactura = new Paragraph("Factura", FontFactory.getFont(FontFactory.HELVETICA_BOLD, 24, Color.BLACK));
@@ -158,6 +72,12 @@ public class FacturaPdfView extends AbstractPdfView {
 		tituloFactura.setSpacingAfter(20f);
 		document.add(tituloFactura);
 		
+		// Agregar barra
+		PdfPTable barra = new PdfPTable(1);
+		barra.setWidthPercentage(200);
+		barra.getDefaultCell().setBackgroundColor(new Color(156, 137, 184));
+		barra.getDefaultCell().setFixedHeight(3);
+		barra.addCell("");
 		document.add(barra);
 		
 		// Crear una tabla con una fila y tres celdas (Se agregarán los datos del cliente en la primera celda, una celda vacía
@@ -167,22 +87,22 @@ public class FacturaPdfView extends AbstractPdfView {
 		table.setSpacingAfter(20f);
 		table.setSpacingBefore(20f);
 		
-		// Párrafo para los datos del cliente
+		// Párrafo para los datos del cliente con la fuente creada
 		Paragraph datosCliente = new Paragraph();
-		datosCliente.add(new Phrase(mensajes.getMessage("text.cliente.nombre") + ": " + factura.getCliente().getNombre()));
+		datosCliente.add(new Phrase(mensajes.getMessage("text.cliente.nombre") + ": " + factura.getCliente().getNombre(), font));
 		datosCliente.add(Chunk.NEWLINE);
-		datosCliente.add(new Phrase(mensajes.getMessage("text.cliente.apellido") + ": "  + factura.getCliente().getApellido()));
+		datosCliente.add(new Phrase(mensajes.getMessage("text.cliente.apellido") + ": "  + factura.getCliente().getApellido(), font));
 		datosCliente.add(Chunk.NEWLINE);
-		datosCliente.add(new Phrase(mensajes.getMessage("text.cliente.email") + ": "  + factura.getCliente().getEmail()));
+		datosCliente.add(new Phrase(mensajes.getMessage("text.cliente.email") + ": "  + factura.getCliente().getEmail(), font));
 		datosCliente.setAlignment(Element.ALIGN_LEFT);
 		datosCliente.setSpacingAfter(20f);
 		
-		// Tabla con los datos de la factura
+		// Tabla con los datos de la factura con la fuente creada
 		PdfPTable tabla2 = new PdfPTable(1);
 		tabla2.setSpacingAfter(20f);
 		tabla2.setHorizontalAlignment(Element.ALIGN_LEFT);
 
-		cell = new PdfPCell(new Phrase(mensajes.getMessage("text.factura.ver.datos.factura")));
+		cell = new PdfPCell(new Phrase(mensajes.getMessage("text.factura.ver.datos.factura"), font));
 		cell.setBackgroundColor(new Color(156, 137, 184));
 		cell.setPadding(8f);
 		cell.setHorizontalAlignment(Element.ALIGN_CENTER);
@@ -191,9 +111,12 @@ public class FacturaPdfView extends AbstractPdfView {
 		cell.setBorderWidth(1f);
 
 		tabla2.addCell(cell);
-		tabla2.addCell(mensajes.getMessage("text.cliente.factura.folio") + ": " + factura.getId());
-		tabla2.addCell(mensajes.getMessage("text.cliente.factura.descripcion") + ": " + factura.getDescripcion());
-		tabla2.addCell(mensajes.getMessage("text.cliente.factura.fecha") + ": " + factura.getCreateAt());
+		PdfPCell cellText = new PdfPCell(new Phrase(mensajes.getMessage("text.cliente.factura.folio") + ": " + factura.getId(), font));
+        tabla2.addCell(cellText);
+        cellText.setPhrase(new Phrase(mensajes.getMessage("text.cliente.factura.descripcion") + ": " + factura.getDescripcion(), font));
+        tabla2.addCell(cellText);
+        cellText.setPhrase(new Phrase(mensajes.getMessage("text.cliente.factura.fecha") + ": " + factura.getCreateAt(), font));
+        tabla2.addCell(cellText);
 
 		// Primera celda: datos del cliente
 		PdfPCell datosClienteCell = new PdfPCell();
@@ -290,5 +213,11 @@ public class FacturaPdfView extends AbstractPdfView {
 		document.add(tablaProductos);
 
 	}
+	
+	@Override
+    protected Document newDocument() {
+        // Establecer el tamaño de página
+        return new Document(PageSize.A4);
+    }
 
 }
